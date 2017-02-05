88542c7dbcd74be4a2a0ce0e6e7638326e15badb
Mike Erickson <codedungeon@gmail.com>
2017-02-04 18:52:43 -0800

    added version bumping

--- tasks/testing.js
+++ tasks/testing.js
@@ -1,8 +1,11 @@
+/*global require*/
+
 const gulp    = require('gulp');
 const phpunit = require('gulp-phpunit');
-const msg     = require('gulp-messenger');
-const chalk   = require('chalk')
+const msg     = require('cd-messenger');
+const chalk   = require('chalk');
 const config  = require('./gulp.config');
+const execa   = require('execa');
 
 const files = config.scripts.server;
 
@@ -14,7 +17,12 @@ gulp.task('test:server', () =>{
 	gulp.src(files)
 		.pipe(phpunit('./vendor/bin/phpunit', options))
 		.on('error', (error) => {
-			//msg.error(error);
+			msg.error(error);
+		});
+
+		// bump prerelease in package.json
+		execa('bash', ['./scripts/bump.sh']).then(result => {
+			console.log(chalk.green.bold(result.stdout));
 		});
 });
 
